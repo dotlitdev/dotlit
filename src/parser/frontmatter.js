c877193f85e91d2d349903dcd2700ef10fb06e92
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-03-08 22:33:01 +0000

    Update frontmatter.js

--- src/parser/frontmatter.js
+++ src/parser/frontmatter.js
@@ -13,24 +13,23 @@ export default function (...args) {
     return (tree,file) => {
         let data = {}
         visit( tree, 'html', (node, index, parent) => {  
-    level(0, log)( '[FrontMatter] now has access to file: ', !!options.file)
-    if (node.value.indexOf(FRONTMATTER_OPEN) === 0 && node.value.indexOf(FRONTMATTER_CLOSE) === (node.value.length - FRONTMATTER_CLOSE.length)) {
-        const yamlString = node.value.slice(FRONTMATTER_OPEN.length, node.value.length - FRONTMATTER_CLOSE.length).trim()
-        console.log("ORIGINAL: ", node.value)
-        console.log("YAML: ", yamlString)
-        const file = options.file
-        const newNode = {
-            type: 'frontmatter',
-            data: yaml.load(yamlString, 'utf8'),
-            value: yamlString,
-            position: node.position
-        }
-        console.log(newNode)
-        data = Object.assign({}, data, newNode.data)
-        node = newNode
-    }
-}
-)
+            level(0, log)( '[FrontMatter] now has access to file')
+            if (node.value.indexOf(FRONTMATTER_OPEN) === 0 && node.value.indexOf(FRONTMATTER_CLOSE) === (node.value.length - FRONTMATTER_CLOSE.length)) {
+               const yamlString = node.value.slice(FRONTMATTER_OPEN.length, node.value.length - FRONTMATTER_CLOSE.length).trim()
+               console.log("ORIGINAL: ", node.value)
+               console.log("YAML: ", yamlString)
+        
+               const newNode = {
+                  type: 'frontmatter',
+                  data: yaml.load(yamlString, 'utf8'),
+                  value: yamlString,
+                  position: node.position
+               }
+               console.log(newNode)
+               data = Object.assign({}, data, newNode.data)
+               node = newNode
+            }
+        })
         file.data = Object.assign({}, file.data, data)
     }
 }
