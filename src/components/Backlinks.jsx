b793d3bc382a81a90a324c5c4a5ed30de023d032
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-03-27 22:13:40 +0000

    Update Backlinks.jsx

--- src/components/Backlinks.jsx
+++ src/components/Backlinks.jsx
@@ -4,7 +4,7 @@ import path from 'path'
 export default class Backlinks extends React.Component {
     render() {
         return <>
-            <h4>Backlinks ({this.props.links.length})</h4>
+            <h4>{`Backlinks (${this.props.links.length})`}</h4>
             <ol>
                 {this.props.links.map( (link) => {
                     return  <li><a title={link.title} href={path.join(this.props.root, link.url)}>{link.title}</a></li>
