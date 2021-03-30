f2eaa8bcf3f8d94e4153b13b7a8fb56f0743ef99
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-03-30 13:07:45 +0100

    Update Link.jsx

--- src/components/base/Link.jsx
+++ src/components/base/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Link = props => {
     const title = props.node.properties.title
-    // console.log("<Link/>", props)
+    console.log("<Link/>", props)
     return <a className={props.className}
         href={props.href}
         title={title}
