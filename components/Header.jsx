8c56c96830ba12d0469d80361c8133ee0675e40e
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-01 23:00:18 +0000

    Deploying to gh-pages from  @ 08c59c11d07406daa31267d75b8e09ae86de3305 🚀

---
+++ components/Header.jsx
@@ -0,0 +1,20 @@
+import react from 'react'
+
+export const Header = props => {
+
+   const [d,l,ex] = [
+     'litDebug',
+     localStorage,
+     '*,fs,client,Cell,sections,etc...',
+   ]
+   const setDebug = ev => {
+        ev.preventDefault()
+        l.setItem( d, prompt(d, l.getItem(d) || ex) )
+        return false
+    }
+    
+    return <header>
+                 <a href={props.root}>Home</a>
+                 <span onClick={setDebug}>Debug</span>
+            </header>
+}
