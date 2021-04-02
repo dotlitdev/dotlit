03f2550e2cbaa88b0f77f49b073dd979a5055c43
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-02 21:36:38 +0000

    Deploying to gh-pages from  @ 55d4311877d70e2373563678e41520544da39e63 🚀

--- utils/unist-util-position-to-node.js
+++ utils/unist-util-position-to-node.js
@@ -1,10 +1,21 @@
 export const positionToNode = (pos,tree) => {
   // TODO: implement
+
   const start = pos.start
     ? pos.start
     : pos.line && pos.column
       ? pos
       : undefined
-  
-  
+
+  const end = pos.end
+    ? pos.end
+    : pos.line && pos.column
+      ? pos
+      : undefined
+
+  if (tree.children) {
+
+  } if (tree.position.start.line >= start.line && tree.position.end.line <= end.line) {
+    return tree
+  }
 }
