6408bb9dcb68487d54b2d1b2ab39b199a95b0f23
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-03 12:19:55 +0000

    Deploying to gh-pages from  @ da09ace24fe1035eaf8f6d0023f59444689f5db6 🚀

---
+++ utils/unist-util-select-position.js
@@ -0,0 +1,14 @@
+import filter from 'unist-util-filter'
+import {selectAll} from 'unist-util-select'
+
+const console = getConsoleForNamespace('util')
+
+export const atPos = pos => (node) => {
+  const pos2 = node.position
+  const startInside = (pos2.start.line >= pos.start.line
+    && pos2.start.line <= pos.end.line)
+  const endInside = (pos2.end.line >= pos.start.line
+     && pos2.end.line <= pos.end.line)
+  console.log("atPos: " + node.type, startInside || endInside ,pos2.start.line, pos2.end.line, startInside, endInside, pos.start.line, pos.end.line)
+  return startInside || endInside
+}
