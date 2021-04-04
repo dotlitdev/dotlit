50c06a472219d73d4632e1207489a592395ff92e
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-05 00:27:22 +0100

    Updated src/components/CodeMeta.jsx

--- src/components/CodeMeta.jsx
+++ src/components/CodeMeta.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 
-export default class CodeMeta extends React.Component {
-    render() {
+export default const CodeMeta = props => {
         return <span className="meta">
-            <span className="lang">{this.props.meta.lang}</span>
-            <span className="repl">{this.props.meta.repl}</span>
-            <span className="filename">{this.props.meta.filename}</span>
+            <span className="lang">{props.meta.lang}</span>
+            <span className="repl">{props.meta.repl}</span>
+            <span className="filename">{props.meta.filename}</span>
 
-            {this.props.meta.tags && <ul className="tags">{
-                this.props.meta.tags.map( (tag, i) => <li key={tag+i}><span className="tag">{tag}</span></li>)
+            {props.meta.tags && <ul className="tags">{
+                props.meta.tags.map( (tag, i) => <li key={tag+i}><span className="tag">{tag}</span></li>)
             }</ul>}
 
-            {this.props.meta.directives && <ul className="directives">{
-                this.props.meta.directives.map( (dir, i) => <li key={dir+1}><span className="directive">{dir}</span></li>)
+            {props.meta.directives && <ul className="directives">{
+                
+props.meta.directives.map( (dir, i) => <li key={dir+1}><span className="directive">{dir}</span></li>)
             }</ul>}
         </span>
-    }
 }
