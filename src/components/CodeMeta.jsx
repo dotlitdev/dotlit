be780a5ef5f9b7a8b546df5d92e0207c45dca577
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-04 23:39:31 +0100

    Updated src/components/CodeMeta.jsx

---
+++ src/components/CodeMeta.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Meta extends React.Component {
+    render() {
+        return <span className="meta">
+            <span className="lang">{this.props.meta.lang}</span>
+            <span className="repl">{this.props.meta.repl}</span>
+            <span className="filename">{this.props.meta.filename}</span>
+
+            {this.props.meta.tags && <ul className="tags">{
+                this.props.meta.tags.map( (tag, i) => <li key={tag+i}><span className="tag">{tag}</span></li>)
+            }</ul>}
+
+            {this.props.meta.directives && <ul className="directives">{
+                this.props.meta.directives.map( (dir, i) => <li key={dir+1}><span className="directive">{dir}</span></li>)
+            }</ul>}
+        </span>
+    }
+}
+module.exports = CodeMeta
