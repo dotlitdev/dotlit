82eb3fce34fba4dcd44853fff3ceca8b65c646b3
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-04 13:24:35 +0100

    Update index.js

--- src/cli/index.js
+++ src/cli/index.js
@@ -24,29 +24,6 @@ function betterDescription({usage, description, examples}) {
     else if (examples.length && typeof examples[0].length) return `${prefix}${examples.map( eg => `${eg[0]}\n${eg[1]}`).join('\n\n')}`
 }
 
-
-async function convert(cmd) {
-    time('convert')
-    const input = vfile.readSync(cmd.file)
-    if (!cmd.format || cmd.format === 'html') {
-        output((await renderToVfileDoc(input, cmd)).contents)
-    } else if (cmd.format === 'json') {
-        parse(input, cmd).then( (file) => output(JSON.stringify(file, null, cmd.prettify ? 2 : undefined)) ) 
-    } else if (cmd.format === 'md') {
-        stringify(input, cmd).then( file => output(file.contents))
-    } else {
-        log(`Unknown/Unhandled format: ${cmd.format}`)
-    }
-    function output(content) {
-        if (!cmd.output || cmd.output === 'stdout') {
-            console.log(content)
-        } else {
-            fs.writeFileSync(cmd.output, content)
-        }
-        timeEnd('convert')
-    }
-}
-
 program
     .storeOptionsAsProperties(true)
     .passCommandToAction(true)
@@ -59,28 +36,6 @@ program
     }))
     .option('-d, --debug <level>', 'Output debugging information', AsInt, 0)
     .helpOption('-h, --help', 'Output usage information')
-
-program
-    .command('convert <file>')
-    .description('Output a .lit document to the given format.')
-    .usage(betterDescription({
-        usage: "[options] <file>",
-        description: "use this command to",
-        examples: [
-            "lit convert -f html myfile.lit",
-            "lit convert --prettify -f json myfile.lit"
-        ]
-    }))
-    .option('-f, --format <extension>', 'Output format', Identity, 'html')
-    .option('-i, --interactive', 'Interactive html')
-    .option('-p, --prettify', 'Prettify the output')
-    .option('-o, --output <path>', 'Output location', Identity, 'stdout')
-    .action((file, cmd) => {
-        cmd.cwd = process.cwd()
-        cmd.file = file
-        cmd.debug = program.debug
-        convert(cmd)
-    })
 
 program
     .command('generate <path>')
@@ -98,11 +53,11 @@ program
     .option('-g, --github-token <token>', 'GitHub Token')
     .option('-u, --github-user <username>', 'GitHub Username')
     .option('-r, --github-repo <repository-name>', 'GitHub Repository name')
-    .option('-s, --github-secret <passphrase>', 'Passphrase to encrypt token with')
     .action((path, cmd)=>{
         cmd.cwd = process.cwd()
         cmd.path = path
         cmd.debug = program.debug
+        console.log("[cli] cmd:", cmd)
         process.env.DEBUG = cmd.debug
         generate(cmd)
     })
