31115c72312c4e702466fe4fa14e7acf385ddef4
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-05 22:10:41 +0100

    Updated src/renderer/index.jsx

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -7,6 +7,7 @@ import ReactDOMServer from 'react-dom/server'
 import React from 'react'
 
 import {processor as parserProcessor} from '../parser'
+import {transcludeCode} from './transcludeCode'
 
 import Document from '../components/Document'
 import Paragraph from '../components/base/Paragraph'
@@ -20,48 +21,15 @@ const console = getConsoleForNamespace('codeblocks')
 
 export function processor(fs) {
     return parserProcessor()
+
+    // transclude codeblocks with source
+    // when available 
+    .use( transcludeCode )
    
     // hoist ast to data
     .use( (...args) => {
          return (tree,file) => {
              file.data.ast = tree
-         }
-     })
-
-    // transclude codeblocks with source
-    // when available 
-    .use( (...args) => {
-         return async (tree,file) => {
-             if(!fs) return;
-             console.log("Checking for files to transclude")
-             for (const block of selectAll("code", tree)) {
-                 if (block.data && block.data.meta && block.data.meta.source) {
-                    const source = block.data.meta.source
-                    console.log("has source", source)
-
-                    if (source.uri) {
-                        const resp = await fetch(source.uri)
-                        if (resp.status >= 200 && resp.status < 400) {
-                            const value = await resp.text()
-                            console.log("has value", value)
-                            block.value = value
-                        }
-                    }
-                    else if (source.filename) {
-                        const filePath = path.join(path.dirname(file.path), source.filename)
-                        console.log("to filePath", filePath)
-
-                        try {
-                            
-                            const value = await fs.readFile(filePath)
-                            console.log("has value", value)
-                            block.value = value
-                        } catch(err) {
-                            file.message("Failed to load " + block.data.meta.fromSource + " as " + filePath)
-                        }
-                    }
-                 }
-             }
          }
      })
 
