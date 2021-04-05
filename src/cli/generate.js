18b861bfb77f2c211910d220a63757f11583bed1
dotlitdev <webmaster@dotlit.org>
2021-04-05 12:19:27 +0100

    refactor header menu and translude components

--- src/cli/generate.js
+++ src/cli/generate.js
@@ -15,6 +15,7 @@ import { diffieHellman } from 'crypto'
 import { decorateLinkNode } from '../parser/links'
 
 import { getConsoleForNamespace } from '../utils/console'
+import { Identity } from '../../build/utils/functions'
 
 const console = getConsoleForNamespace('generate')
 
@@ -70,7 +71,7 @@ export function generate(cmd) {
     console.log(`Generating from path: ${cmd.path} (${globAll})`)
     console.log(`Output path: ${cmd.output} cwd: ${cmd.cwd}`)
     
-    function process(done) {
+    function processFilesystem(done) {
         time('generate')
 
         glob(globAll, {cwd: `${cmd.path}/`, ignore}, async (err, matches) => {
@@ -98,28 +99,37 @@ export function generate(cmd) {
                     })
                 })
 
-                let ast_files_prelinks = await Promise.all(src_files.map( async file => await renderProcessor(fs).process(await file)))
-                const [ast_files, manifest] = generateBacklinks(ast_files_prelinks, cmd.output)
-
-                const html_files = await Promise.all(ast_files.map( async file => {
-                    await fs.writeFile(path.join(cmd.output, file.path + '.json'), JSON.stringify(file.data.ast, null, 4))
-                    const html_file = await renderedVFileToDoc(await file, cmd)
-                    await fs.writeFile(path.join(cmd.output, file.path), file.contents)
-                    console.log(`Wrote  ${file.path} to "${path.join(cmd.output, file.path)}" to disk`)
-
-                    for (const codefile of html_file.data.files) {
-                        const filename = codefile.data && codefile.data.meta && codefile.data.meta.filename
-                        if (filename) {
-                            const filepath = path.join(cmd.output, path.dirname(file.path), filename)
-                            await fs.writeFile(filepath, codefile.value)
-                            console.log(`Wrote codefile ${filename} to "${filepath}" on disk`)
-                        }
+                let ast_files_prelinks = await Promise.all(src_files.map( async file => {
+                    try {
+                        const fetchedFile = await file
+                        const renderedFile = await renderProcessor(fs).process(fetchedFile)
+                        return renderedFile
+                    } catch (err) {
+                        console.error(`Failed to process ${file.path}`)
                     }
-                    return html_file;
+                }))
+                const [ast_files, manifest] = generateBacklinks(ast_files_prelinks, cmd.output)
+                const html_files = await Promise.all(ast_files.map( async file => {
+                    try {
+                        await fs.writeFile(path.join(cmd.output, file.path + '.json'), JSON.stringify(file.data.ast, null, 4))
+                        const html_file = await renderedVFileToDoc(await file, cmd)
+                        await fs.writeFile(path.join(cmd.output, file.path), file.contents)
+                        console.log(`Wrote  ${file.path} to "${path.join(cmd.output, file.path)}" to disk`)
+
+                        for (const codefile of html_file.data.files) {
+                            const filename = codefile.data && codefile.data.meta && codefile.data.meta.filename
+                            if (filename) {
+                                const filepath = path.join(cmd.output, path.dirname(file.path), filename)
+                                await fs.writeFile(filepath, codefile.value)
+                                console.log(`Wrote codefile ${filename} to "${filepath}" on disk`)
+                            }
+                        }
+                        return html_file;
+                    } catch (err) { console.error(`Failed to process ${file.path}`)}
                 }))
                 
                 await fs.writeFile(path.join(cmd.output, 'manifest.json'), JSON.stringify(manifest, null, 4))
-                console.log(`Wrote ${html_files.length} file(s) to disk`)
+                console.log(`Wrote ${html_files.filter(Identity).length}/${html_files.length} .lit file(s) to disk`)
 
                 await fs.copyFile( path.join(__dirname,'../../dist/web.bundle.js'), path.join(cmd.output, 'web.bundle.js'))
                 await fs.copyFile( path.join(__dirname,'../../dist/web.bundle.js.map'), path.join(cmd.output, 'web.bundle.js.map'))
@@ -135,13 +145,13 @@ export function generate(cmd) {
         })
     }
 
-    process(NoOp)
+    processFilesystem(NoOp)
 
     if (cmd.watch) {
         console.log(`Watching "${cmd.path}" for changes...`)
         watch([`${cmd.path}/${globAll}`], function(done){
             console.log(`Change detected in "${cmd.path}".`)
-            process(done)
+            processFilesystem(done)
         })
     }
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Identity } from '../utils/functions'
+import { getConsoleForNamespace } from '../utils/console'
+
+const console = getConsoleForNamespace('Header')
+
+const setDebug = ev => {
+  ev.preventDefault()
+  const key = 'litDebug'
+  const example = '*,fs,client,Cell,sections,etc...'
+  const storage = typeof localStorage !== 'undefined' && localStorage
+  const val = prompt(key, storage.getItem(key) || example)
+  storage.setItem( key, val )
+  console.log(`Set ${key} to "${val}"`)
+  return false
+}
+
+const showInspector = ev => {
+  console.log('Show mobile inspector')
+}
+
+const Menu = props => {
+  console.log('<Menu/>', props.title, props)
+
+  const [open, setOpen] = useState(props.horizontal)
+  const toggleOpen = _ => setOpen(!open)
+
+  const classes = [
+    props.horizontal ? 'horizontal' : null,
+    open ? 'open' : null,
+  ].filter(Identity).join(' ')
+
+  const handleClickTitle = ev => {
+    if (props.onClick) props.onClick()
+    else if (props.href) location.href = props.href 
+    else toggleOpen()
+  }
+
+  const disabled = !props.onClick && !props.href && !props.children
+
+  return <menu className={classes} disabled={disabled}>
+    <li className={"MenuTitle"} key="menu-title" onClick={handleClickTitle} >
+      { props.href
+        ? <a href={props.href}>{props.title}</a>
+        : props.title}
+      { open && props.horizontal && <span>|</span> }
+    </li>
+    { open && <li className="MenuItems">{ props.children }</li> }
+  </menu>
+}
+
+
 
 export const Header = props => {
-
-   const [d,l,ex] = [
-     'litDebug',
-     typeof localStorage !== 'undefined' && localStorage,
-     '*,fs,client,Cell,sections,etc...',
-   ]
-   const setDebug = ev => {
-        ev.preventDefault()
-        l.setItem( d, prompt(d, l.getItem(d) || ex) )
-        return false
-    }
-    
-    return <header>
-                 <a href={props.root}>Home</a>
-                 <span onClick={setDebug}>Debug</span>
-            </header>
+  console.log('<Header/>', props)
+  return <Menu title="Home" horizontal href={props.root}>
+    <Menu title="File">
+      <span disabled>New</span>
+      <span disabled>Open</span>
+      <span disabled>Save</span>
+      <span disabled>Reset</span>
+      <span disabled>Delete</span>
+    </Menu>
+    <Menu title="Cell">
+      <span disabled>Add</span>
+      <span disabled>Remove</span>
+      <span disabled>Edit</span>
+      <span disabled>Execute</span>
+      <span disabled>Reset</span>
+      <Menu title="Move">
+        <span>Up</span>
+        <span>Down</span>
+      </Menu>
+      <Menu title="Copy">
+        <span>Source</span>
+        <span>Cell</span>
+      </Menu>
+    </Menu>
+    <Menu title="Section">
+      <span disabled>Collapse</span>
+      <span disabled>Remove</span>
+      <Menu title="Move">
+        <span>Up</span>
+        <span>Down</span>
+      </Menu>
+    </Menu>
+    <Menu title="Help">
+      <span onClick={setDebug}>Debug</span>
+    </Menu>
+  </Menu>
+  
 }
