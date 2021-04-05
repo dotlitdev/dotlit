869103419d64dcd9043a16e19afdd0b443ded768
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-05 22:23:40 +0000

    Deploying to gh-pages from  @ 7f9d2c8cfc864d92217b138487e67b2cfb6c6d15 🚀

--- components/CodeMeta.jsx
+++ components/CodeMeta.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 
-export const CodeMeta = props => {
+export const CodeMeta = ({meta}) => {
         return <span className="meta">
-            <span className="lang">{props.meta.lang}</span>
-            <span className="repl">{props.meta.repl}</span>
-            <span className="filename">{props.meta.filename}</span>
-
-            { props.meta.fromSource && <span className="source">{'&gt; ' + props.meta.fromSource}</span> }
-
-            {props.meta.tags && <ul className="tags">{
-                props.meta.tags.map( (tag, i) => <li key={tag+i}><span className="tag">{tag}</span></li>)
+            <span className="lang">{meta.lang}</span>
+            {meta.repl && <span className="repl">{meta.repl}</span> }
+            {meta.filename && <span className="filename">{meta.filename}</span>}
+            {meta.fromSource && <span className="source">{'&gt; ' + meta.fromSource}</span> }
+            {meta.tags && <ul className="tags">{
+               meta.tags.map( (tag, i) => <li key={tag+i}><span className="tag">{tag}</span></li>)
             }</ul>}
-
-            {props.meta.directives && <ul className="directives">{
-                
-props.meta.directives.map( (dir, i) => <li key={dir+1}><span className="directive">{dir}</span></li>)
+            {meta.directives && <ul className="directives">{
+                meta.directives.map( (dir, i) => <li key={dir+1}><span className="directive">{dir}</span></li>)
             }</ul>}
         </span>
 }
