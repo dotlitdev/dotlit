c33c9a46bc28337849e2b17910a122c9bedbd524
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-05 23:08:53 +0000

    Deploying to gh-pages from  @ a51127763e4ac3dedca86f72ad63189a83ca4b78 🚀

--- utils/functions.js
+++ utils/functions.js
@@ -1,6 +1,8 @@
 export const NoOp = () => {}
 export const Identity = x => x
 export const AsInt = x => parseInt(x)
+export const Undef = x => typeof x === 'undefined'
+
 export const getMeta = (key,def) => {
     const el = document.querySelector(`meta[name="lit${key}"]`)
     const val = el ? el.getAttribute('value') : def
