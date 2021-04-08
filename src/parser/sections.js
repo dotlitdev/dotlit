9364108fe3d1938d430fb1b54acff3ed9907a3bb
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-08 22:35:49 +0100

    Update sections.js

--- src/parser/sections.js
+++ src/parser/sections.js
@@ -188,6 +188,7 @@ export const sections = (...args) => (tree) => {
         }
         console.log(`[Sections II] child index: ${i}, type: ${nextNode.type} depth: ${nextNode.depth} id: ${nextNode.data && (nextNode.data.id || nextNode.data.name)}`)
         section.children.push(nextNode)
+        section.position.end = nextNode.position.end
         parent.children[i].processed = true
       }
       headings++
