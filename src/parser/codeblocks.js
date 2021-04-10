ecacbcb0b8c2425ef691b572686a746ed4a1b652
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-10 14:10:18 +0100

    Update codeblocks.js

--- src/parser/codeblocks.js
+++ src/parser/codeblocks.js
@@ -67,7 +67,7 @@ function parseMeta (node) {
     meta.hasOutput = !!output
     meta.hasSource = !!source
     meta.source = source
-    meta.raw = node.meta
+    meta.raw = raw
     if (source) meta.fromSource = source.filename || source.uri
 
     return meta
