4634999b866cf0d3dfde83473ad5c557b36c2283
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-10 23:07:49 +0100

    Update package.js

--- src/package.js
+++ src/package.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 const pkg = require('../package.json')
 
-pkg.bin = { dotlit: './node.bundle.js' }
+pkg.bin = { dotlit: './index.js' }
 pkg.dependencies = {}
 
 const pkgSrc = JSON.stringify(pkg,null,2)
 console.log(pkg)
+
+const indexSrc = "#!/usr/bin/env node\nrequire('./node.bundle.js')\n"
+console.log(indexSrc)
+
 fs.writeFileSync('./dist/package.json', pkgSrc, {encoding: 'utf8'})
+fs.writeFileSync('./dist/index.js', indexSrc, {encoding: 'utf8'})
