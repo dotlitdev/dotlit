fcda0283b1cf100f327c2bdfdba6108c2d579388
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-10 22:20:08 +0100

    Update package.js

--- src/package.js
+++ src/package.js
@@ -6,4 +6,4 @@ pkg.dependencies = {}
 
 const pkgSrc = JSON.stringify(pkg,null,2)
 console.log(pkg)
-fs.writeFileSync('../dist/package.json', pkgSrc, {encoding: 'utf8'})
+fs.writeFileSync('./dist/package.json', pkgSrc, {encoding: 'utf8'})
