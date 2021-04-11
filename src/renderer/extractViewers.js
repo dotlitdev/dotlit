31c5e80630e49f22329bcf21c31f737e1f668f84
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-11 13:59:19 +0100

    Update extractViewers.js

--- src/renderer/extractViewers.js
+++ src/renderer/extractViewers.js
@@ -16,7 +16,11 @@ export const extractViewers = ({fs} = {}) => {
                 console.log('Found Viewer', block)
                 file.data = file.data || {}
                 file.data.viewers = file.data.viewers || {}
-                file.data.viewers[block.data.meta.of] = block
+                try {
+                    file.data.viewers[block.data.meta.of] = await import(`data:text/javascript;base64,${ btoa(block.value)}`)
+                } catch(err) {
+                    console.log("Failed to init viewer", err)
+                }
             }
         }
     }
