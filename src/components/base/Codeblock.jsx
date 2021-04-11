77133b531886f8a0e2c2c784b7fcaa96fae55de9
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-11 23:02:39 +0100

    Update Codeblock.jsx

--- src/components/base/Codeblock.jsx
+++ src/components/base/Codeblock.jsx
@@ -2,26 +2,27 @@ import React from 'react'
 import {log, level} from '../../utils/console'
 import { getConsoleForNamespace } from '../../utils/console'
 import Highlight from 'react-highlight.js'
-
+import SelectionContext from '../SelectionContext'
 import {getViewer} from '../../renderer/Viewers'
 import {CodeMeta} from '../CodeMeta'
 
 const console = getConsoleForNamespace('codeblocks')
 
-export default class Codeblock extends React.Component {
-    render() {
+export const Codeblock = props => {
         
-        const codeNode = this.props.node.children
-                            && this.props.node.children.length == 1
-                            && this.props.node.children[0].tagName === 'code'
-                            ? this.props.node.children[0] 
+        const codeNode = props.node.children
+                            && props.node.children.length == 1
+                            && props.node.children[0].tagName === 'code'
+                            ? props.node.children[0] 
                             : null;
         const meta = codeNode ? codeNode.properties.meta : null
         const viewer = getViewer(meta)
 
+        return <SelectionContext.Consumer>
+        { ctx => {
        
         if (codeNode) {const source = codeNode.children[0].value
-            console.log("[Codeblock]", meta)
+            console.log("[Codeblock]", meta,ctx)
             return <codecell>
                 { meta && <CodeMeta meta={meta}/> }
                 { viewer 
@@ -32,7 +33,8 @@ export default class Codeblock extends React.Component {
             </codecell>
         } else {
             console.log("Default codeblock", this.props.node.children[0])
-            return <codecell><pre>{this.props.children}</pre></codecell>
+            return <codecell><pre>{props.children}</pre></codecell>
         }
-    }
+     }
+   }</SelectionContext.Consumer>
 }
