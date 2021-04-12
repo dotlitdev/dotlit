736ffddbb8543d8071d90f0ea4f86c9cb0518ecf
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-12 16:22:55 +0000

    Deploying to gh-pages from  @ 0f72c7987b9b4670cdbb66bb30370a289477ec64 🚀

--- components/CodeMeta.jsx
+++ components/CodeMeta.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
-export const CodeMeta = ({meta}) => {
+export const CodeMeta = ({meta, toggleFullscreen, toggleLocalRemote}) => {
+
         return <span className="meta">
             <span className="lang">{meta.lang}</span>
             {meta.repl && <span className="repl">{meta.repl}</span> }
             {meta.filename && <span className="filename">{meta.filename}</span>}
-            {meta.fromSource && <span className="source">{'< ' + meta.fromSource}</span> }
+            {meta.fromSource && <span onClick={toggleLocalRemote} className="source">{'< ' + meta.fromSource}</span> }
             {meta.tags && <ul className="tags">{
                meta.tags.map( (tag, i) => <li key={tag+i}><span className="tag">{tag}</span></li>)
             }</ul>}
             {meta.directives && <ul className="directives">{
-                meta.directives.map( (dir, i) => <li key={dir+1}><span className="directive">{dir}</span></li>)
+                meta.directives.map( (dir, i) => {
+                    const onClick = dir === 'inline' ? toggleFullscreen : null
+                    return <li key={dir+1} onClick={onClick}><span className="directive">{dir}</span></li>
+                })
             }</ul>}
         </span>
 }
