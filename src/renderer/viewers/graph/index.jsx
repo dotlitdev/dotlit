2b289ebdab94be918c45254f43a95b6fe86f882b
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-16 21:04:12 +0100

    Create index.jsx

---
+++ src/renderer/viewers/graph/index.jsx
@@ -0,0 +1,40 @@
+export const viewer = ({node, React}) => {
+   const c = React.createElement
+   const data = node.value
+   const el = c('iframe', {srcDoc: `<!DOCTYPE html>
+<head>
+  <style> body { margin: 0; } </style>
+  <script src="https://unpkg.com/force-graph"></script>
+  <!-- from: https://github.com/vasturiano/force-graph -->
+</head>
+<body>
+  <div id="graph"></div>
+  <script>
+    const data = ${data};
+    const thisfile = window.parent.lit.location.src
+    const elem = document.getElementById('graph')
+    const Graph = ForceGraph()(elem)
+        .graphData(data)
+        .nodeId('id')
+        .nodeVal((n) => n.backlinks ? n.backlinks.length : 1)
+        .nodeLabel('id')
+        .nodeAutoColorBy('type')
+        // .linkColor(d => data.nodes[d.source].exists ? 'red' : 'lightblue')
+        .linkDirectionalArrowLength(6)
+        .onNodeHover(node => elem.style.cursor = node ? 'pointer' : null)
+        .onNodeClick(node => {
+          // Center/zoom on node
+          Graph.centerAt(node.x, node.y, 1000);
+          Graph.zoom(2, 2000);
+        });
+
+    setTimeout( () => {
+        const { nodes, links } = Graph.graphData()
+        const node = nodes.filter( n => n.id === thisfile)[0]
+        Graph.centerAt(node.x, node.y, 1000);
+        Graph.zoom(2, 1000);
+    }, 2000)
+  </script>
+</body>`})
+   return el
+  }
