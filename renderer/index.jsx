fabc4903916095f9a79b586297c24fcb98525174
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-16 08:42:59 +0000

    Deploying to gh-pages from  @ d4612d39db84e20719dba064789f16ec7d511f50 🚀

--- renderer/index.jsx
+++ renderer/index.jsx
@@ -3,19 +3,24 @@ import remark2rehype from 'remark-rehype'
 import rehype2react from 'rehype-react'
 import {selectAll} from 'unist-util-select'
 
+import hastCodeHandler from './utils/hast-util-code-handler'
+
 import ReactDOMServer from 'react-dom/server'
 import React from 'react'
 
 import {processor as parserProcessor} from '../parser'
 import {transcludeCode} from './transcludeCode'
+import {extractViewers} from './extractViewers'
 
 import Document from '../components/Document'
 import Paragraph from '../components/base/Paragraph'
 import Link from '../components/base/Link'
-import Codeblock from '../components/base/Codeblock'
+import {Codeblock} from '../components/base/Codeblock'
 import Cell from '../components/Cell'
+import { Section } from '../components/Section'
 
 import { getConsoleForNamespace } from '../utils/console'
+
 
 const console = getConsoleForNamespace('codeblocks')
 
@@ -25,54 +30,47 @@ export function processor(fs) {
     // hoist ast to data
     .use( (...args) => {
          return (tree,file) => {
+             console.log("[Hoist AST data]")
              file.data.ast = tree
          }
      })
 
     // transclude codeblocks with source
     // when available 
-    .use( (...args) => {
-         return async (tree,file) => {
-             if(!fs) return;
-             console.log("Checking for files to transclude")
-             for (const block of selectAll("code", tree)) {
-                 if (block.data && block.data.meta && block.data.meta.source) {
-                    const source = block.data.meta.source
-                    console.log("has source", source)
+    .use( transcludeCode, {fs} )
 
-                    if (source.uri) {
-                        const resp = await fetch(source.uri)
-                        if (resp.status >= 200 && resp.status < 400) {
-                            const value = await resp.text()
-                            console.log("has value", value)
-                            block.value = value
-                        }
-                    }
-                    else if (source.filename) {
-                        const filePath = path.join(path.dirname(file.path), source.filename)
-                        console.log("to filePath", filePath)
-
-                        try {
-                            
-                            const value = await fs.readFile(filePath)
-                            console.log("has value", value)
-                            block.value = value
-                        } catch(err) {
-                            file.message("Failed to load " + block.data.meta.fromSource + " as " + filePath)
-                        }
-                    }
-                 }
-             }
-         }
-     })
+    // extract custom viewers
+    .use( extractViewers )
 
     // extract files to data
     .use( (...args) => {
          return (tree,file) => {
+             console.log("[Extact files]")
              file.data.files = selectAll("code", tree)
          }
      })
-    .use(remark2rehype, {allowDangerousHtml: true})
+
+    // hoist mdast data to hast data
+    // Dosabled as failed to process due to JSON stringify error
+    .use( (...args) => {
+         return (tree,file) => {
+
+             console.log("[Hoist mdast data] disabled")
+             for (const code of selectAll("code", tree)) {
+                 if (false && code.data) {
+                     code.data.hProperties = code.data.hProperties || {}
+                     code.data.hProperties.data = code.data
+                 }
+             }
+         }
+     })
+    .use(remark2rehype, {
+        allowDangerousHtml: true,
+        // passThrough: ['mdcode'],
+        handlers: {
+            code: hastCodeHandler,
+        },
+     })
     .use(rehype2react, {
         Fragment: React.Fragment,
         allowDangerousHtml: true,
@@ -83,6 +81,7 @@ export function processor(fs) {
             a: Link,
             pre: Codeblock,
             cell: Cell,
+            section: Section
         }
     })
 }
