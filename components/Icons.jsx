9969669c350dabda8392327535bdcbd7d1d887b1
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-17 13:04:20 +0000

    Deploying to gh-pages from  @ c35c62d18723e3f6e7c3f08f8d672d931ed45847 🚀

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faPlay, faEdit, faTimes, faSave } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faPlay, faEdit, faExternalLinkAlt, faTimes, faSave } from '@fortawesome/free-solid-svg-icons'
 
-export const Icon = (props) => {
-  return <FontAwesomeIcon icon={props.name} />
-}
+
+export const Icon = (props) => <FontAwesomeIcon icon={props.icon} />
 
 export const MenuIcon = props => (<FontAwesomeIcon icon={faBars} {...props} />)
 export const EditIcon = props => (<FontAwesomeIcon icon={faEdit} {...props} />)
 export const ExecIcon = props => (<FontAwesomeIcon icon={faPlay} {...props} />)
 export const CloseIcon = props => (<FontAwesomeIcon icon={faTimes} {...props} />)
 export const SaveIcon = props => (<FontAwesomeIcon icon={faSave} {...props} />)
+
+export const ExternalLinkIcon = props => <Icon icon={faExternalLinkAlt}/>
