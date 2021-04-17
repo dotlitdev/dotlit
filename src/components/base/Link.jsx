f76f2c1c844bbcbd0fe92da55af2c1284798e7ff
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-17 14:31:23 +0100

    Update Link.jsx

--- src/components/base/Link.jsx
+++ src/components/base/Link.jsx
@@ -6,10 +6,11 @@ const console = getConsoleForNamespace('Link')
 
 const Link = props => {
     const title = props.node.properties.title
+    const data = props.data
     const wikilink = props.wikilink ? 'true' : undefined
-    const icon = wikilink 
-                 ? null
-                 : <ExternalLinkIcon/>
+    const icon = (data && data.isAbsolute) 
+                 ? <ExternalLinkIcon/>
+                 : null
 
     console.log("<Link/>", title, props)
 
