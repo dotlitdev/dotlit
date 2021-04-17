57da4e28185b4370c220a690b1d682da61c61d24
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-17 13:25:26 +0100

    Update Link.jsx

--- src/components/base/Link.jsx
+++ src/components/base/Link.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Icon } from './Icons'
 import { getConsoleForNamespace } from '../../utils/console'
 
 const console = getConsoleForNamespace('Link')
 
 const Link = props => {
     const title = props.node.properties.title
-    // console.log("<Link/>", props)
+    const icon = ''
+
+    console.log("<Link/>", title, props)
+
     return <a className={props.className}
         href={props.href}
         title={title}
         wikilink={props.wikilink ? 'true' : undefined}>
+            { icon && <Icon name={icon}/>
             {props.children}
         </a>
 }
