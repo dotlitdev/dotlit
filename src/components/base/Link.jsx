0b092990f42107f05493dbbee259a0200aac0064
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-17 13:34:48 +0100

    Update Link.jsx

--- src/components/base/Link.jsx
+++ src/components/base/Link.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon } from './Icons'
+import { Icon } from '../Icons'
 import { getConsoleForNamespace } from '../../utils/console'
 
 const console = getConsoleForNamespace('Link')
