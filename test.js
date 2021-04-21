a8a0341d663bf6f3df2e601cc09400c72369b925
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-21 23:47:52 +0000

    Deploying to gh-pages from  @ c32a3cf0d04e2b61f2abf59c1afd4b24159bee5d 🚀

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 const wait = (ms) => {
   return new Promise(resolve => {
-    setTimeout( () => resolve(new Date()), ms)
+    setTimeout( () => {
+      resolve(new Date())
+    }, ms)
+     
   })
 }
 return wait(1000)
+
 
 
