072beca1eb66afe10bac1f6f387d0aaadb2758bc
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-22 22:51:58 +0000

    Deploying to gh-pages from  @ 48a7df1ea03161b7f5d977a5c67162734615dfc5 🚀

--- test.jsx
+++ test.jsx
@@ -1,39 +1,10 @@
 return (async args => {
 
-const b = document.body
-const ch = b.clientHeight
-const sh = b.scrollHeight
-const st = b.scrollTop
+  const React = await import('https://cdn.skypack.dev/react')
 
-const c = (ch/2)+st
-const cp = c / sh
-  const y = cp
-  const confetti = (await 
-  import('https://cdn.skypack.dev/canvas-confetti')).default
-  // do this for 30 seconds
-var duration = 30 * 1000;
-var end = Date.now() + duration;
-
-(function frame() {
-  // launch a few confetti from the left edge
-  confetti({
-    particleCount: 7,
-    angle: 90,
-    spread: 55,
-    origin: { y: 0}
-  });
-  // and launch a few from the right edge
-  confetti({
-    particleCount: 7,
-    angle: 180,
-    spread: 55,
-    origin: { y: 1 }
-  });
-
-  // keep going until we are out of time
-  if (Date.now() < end) {
-    console.log("still going")
-    requestAnimationFrame(frame);
+  const Component = props => {
+    return <div {...props}>React component</div>
   }
-}());
+
+  return <Component id="foo"/>
 })()
