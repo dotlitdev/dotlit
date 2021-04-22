f425fc06e01c65db12627f86d7c355a4ecc442ae
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-22 21:45:57 +0000

    Deploying to gh-pages from  @ 197619daee37572ed75a50c83f8f874637203455 🚀

--- test.jsx
+++ test.jsx
@@ -1,7 +1,7 @@
 return (async args => {
 
-  const confetti = (await import("https://cdn.jsdelivr.net/npm/canvas-confetti@0.2.0-beta0/dist/confetti.module.mjs")
-  // import('https://cdn.skypack.dev/canvas-confetti')).default
+  const confetti = (await import("https://cdn.jsdelivr.net/npm/canvas-confetti@0.2.0-beta0/dist/confetti.module.mjs")).default
+  // import('https://cdn.skypack.dev/canvas-confetti')
 
   return confetti()
   return "yay"
