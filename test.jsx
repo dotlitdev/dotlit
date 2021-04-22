b0ca989a9f24974e13899cb3fa846e18c30cea26
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-22 22:44:33 +0000

    Deploying to gh-pages from  @ 2f67a3d1a03f1825c62da6a814a23a1720f14ecb 🚀

--- test.jsx
+++ test.jsx
@@ -1,8 +1,39 @@
 return (async args => {
 
-  const confetti = await 
-  import('https://cdn.skypack.dev/canvas-confetti')
+const b = document.body
+const ch = b.clientHeight
+const sh = b.scrollHeight
+const st = b.scrollTop
 
-  confetti.default({y: Math.random()})
-  return "yay " + Math.random()
+const c = (ch/2)+st
+const cp = c / sh
+  const y = cp
+  const confetti = (await 
+  import('https://cdn.skypack.dev/canvas-confetti')).default
+  // do this for 30 seconds
+var duration = 30 * 1000;
+var end = Date.now() + duration;
+
+(function frame() {
+  // launch a few confetti from the left edge
+  confetti({
+    particleCount: 7,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 }
+  });
+  // and launch a few from the right edge
+  confetti({
+    particleCount: 7,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 }
+  });
+
+  // keep going until we are out of time
+  if (Date.now() < end) {
+    console.log("still going")
+    requestAnimationFrame(frame);
+  }
+}());
 })()
