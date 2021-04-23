4a7d8b7e5bab5f09ff6e378fb55ab71ea449e315
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-23 21:51:53 +0000

    Deploying to gh-pages from  @ 27dafd237859f8ece8314b733f64138243e025dd 🚀

--- components/CodeMeta.jsx
+++ components/CodeMeta.jsx
@@ -18,13 +18,18 @@ export const CodeMeta = ({meta, toggleFullscreen, toggleLocalRemote, toggleColla
             {meta.repl && <span className="repl">{meta.repl}</span> }
             {meta.filename && <span className="filename">{meta.filename}</span>}
             {meta.fromSource && <span onClick={toggleLocalRemote} className="source">{'< ' + meta.fromSource}</span> }
+            {meta.hasOutput && <span className="output">{'> ' + meta.output}</span> }
             {meta.tags && <ul className="tags">{
-               meta.tags.map( (tag, i) => <li key={tag+i}><span className="tag">{tag}</span></li>)
+               meta.tags.map( (tag, i) => <li key={tag+i} style={colorStyle(tag)}>
+                   <span className="tag">{tag}</span>
+               </li>)
             }</ul>}
             {meta.directives && <ul className="directives">{
                 meta.directives.map( (dir, i) => {
                     const onClick = dir === 'inline' ? toggleFullscreen : null
-                    return <li key={dir+1} onClick={onClick}><span className={`directive dir-${dir}`}>{dir}</span></li>
+                    return <li key={dir+1} onClick={onClick} style={colorStyle(dir)}>
+                       <span className={`directive dir-${dir}`}>{dir}</span>
+                    </li>
                 })
             }</ul>}
             { meta.updated && <span className="updatedAt">Updated <Time ms={parseInt(meta.updated)} /></span> }
