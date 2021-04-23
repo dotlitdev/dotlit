58837377877ae0798cd719df163d79622c05194b
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-23 11:46:14 +0000

    Deploying to gh-pages from  @ 56c876cd4d67817c7b1b760cd338d72489dd0a48 🚀

--- test.jsx
+++ test.jsx
@@ -3,12 +3,12 @@ return (async args => {
   const React = (await import('https://cdn.skypack.dev/react')).default
   const { useState } = React
   
-  const Component = props => {
+  const Clicker = props => {
     const [c,setC] = useState(0)
     const click = e => setC(c+1)
 
     return <div onClick={click}>{ "Click count: " + c}</div>
   }
 
-  return <Component/>
+  return <Clicker/>
 })()
