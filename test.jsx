d513503ef924dcc8b817666ab2fab1b18cb14ebf
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-23 11:44:24 +0000

    Deploying to gh-pages from  @ 970e57147614ffe40b4bdcd5853effe8adc91753 🚀

--- test.jsx
+++ test.jsx
@@ -1,10 +1,10 @@
 return (async args => {
 
   const React = await import('https://cdn.skypack.dev/react')
+  const { useState } = React
   
   const Component = props => {
-    const u = React.useState
-    const [c,setC] = u(0)
+    const [c,setC] = useState(0)
     const click = e => setC(c+1)
 
     return <div onClick={click}>{ "Click count: " + c}</div>
