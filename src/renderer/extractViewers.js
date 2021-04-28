2141b3a4e26375a1be72272d3ccb1d61b0a75329
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-28 21:41:42 +0100

    Update extractViewers.js

--- src/renderer/extractViewers.js
+++ src/renderer/extractViewers.js
@@ -4,16 +4,16 @@ import { selectAll } from 'unist-util-select'
 import {btoa, atob } from '../utils/safe-encoders'
 
 const console = getConsoleForNamespace('extractViewers')
-
-function requireFromString(src, filename) {
-    var Module = module.constructor;
-    var m = new Module();
-    m._compile(src, filename);
-    return m.exports;
-  }
+   
 
 const extractModule = async (src, filename) => {
-    return requireFromString(src, filename)
+    var Module = module.constructor;
+    var m = new Module();
+
+    if (typeof m._compile === 'function') {
+        m._compile(src, filename);
+        return m.exports
+    }
     return await import(/* webpackIgnore: true */ `data:text/javascript;base64,${ btoa(block.value)}`)
 }
 
