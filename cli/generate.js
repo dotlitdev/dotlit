913b3534b614f9e734215f95f5ab625789437643
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-29 12:45:07 +0000

    Deploying to gh-pages from  @ edbfd906c37fb78bca8a9fb04b59eacdb1306f96 🚀

--- cli/generate.js
+++ cli/generate.js
@@ -161,11 +161,9 @@ export function generate(cmd) {
                 let ast_files_prelinks = await Promise.all(src_files.map( async file => {
                     try {
                         const fetchedFile = await file
-                        const src = file.contents
-                        const srcPath = file.path
+                       
                         const renderedFile = await renderProcessor(fs).process(fetchedFile)
-                        renderedFile.data.src = src
-                        renderedFile.data.srcPath = srcPath
+                       
                         return renderedFile
                     } catch (err) {
                         console.error(`Failed to process ${file.path}`, err)
@@ -176,7 +174,7 @@ export function generate(cmd) {
                     try {
                         if(file && file.data && file.data.frontmatter && file.data.frontmatter.private) return;
 
-                        await fs.writeFile(path.join(cmd.output, file.data.srcPath), file.data.src)
+                        await fs.writeFile(path.join(cmd.output, file.path), file.contents)
                         await fs.writeFile(path.join(cmd.output, file.path + '.json'), JSON.stringify(file.data.ast, null, 4))
                         const html_file = await renderedVFileToDoc(await file, cmd)
                         await fs.writeFile(path.join(cmd.output, file.path), file.contents)
