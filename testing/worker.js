a7d6ea49559b79a474997b4be4c89f6a6d905151
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-30 11:26:51 +0000

    Deploying to gh-pages from  @ 4d8a87a17b06179cf699fe92d1dd32fa23707a8b 🚀

--- testing/worker.js
+++ testing/worker.js
@@ -1,3 +1,6 @@
+// gross hack around one of codemirror/view bugs
+let document = { documentElement: { style: {} } }
+
 importScripts('../web.bundle.js')
 postMessage("dotlit: " + typeof dotlit)
 console.log("anyone here?")
