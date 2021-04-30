bf6bf11bf480d7b49c8a2a12d6c0c046dc4d22cc
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-04-30 15:01:35 +0000

    Deploying to gh-pages from  @ 4616df0b02a916aa0a2b8917d3e1f53a9ddb4ffc 🚀

--- testing/worker.js
+++ testing/worker.js
@@ -1,6 +1,21 @@
-// gross hack around one of codemirror/view bugs
+// gross hack around one of @codemirror/view bugs
 let document = { documentElement: { style: {} } }
 
+postMessage("Location: " +  typeof location + JSON.stringify(location))
 importScripts('../web.bundle.js')
 postMessage("dotlit: " + typeof dotlit)
-console.log("anyone here?")
+postMessage("lit" + JSON.stringify(dotlit.lit.location))
+
+const getFsTest = async (filepath) => {
+    postMessage(`Getting ${filepath} from local fs.`)
+    try {
+        const file = await dotlit.lit.fs.readFile(filepath)
+        postMessage("length+" + file.length)
+        postMessage('done')
+    } catch(err) {
+        postMessage("error:" + err.toString())
+        postMessage('done')
+    }
+}
+
+getFsTest('/index.lit')
