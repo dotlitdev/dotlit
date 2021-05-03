0e05261a665950afc53ad77907b1fb284109d4b7
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-03 22:35:54 +0100

    Updated src/testing/worker.js

--- src/testing/worker.js
+++ src/testing/worker.js
@@ -1,6 +1,6 @@
 // gross hack around one of @codemirror/view bugs
 let document = { documentElement: { style: {} } }
-
+postMessage("Version: 2")
 postMessage("Location: " +  typeof location + JSON.stringify(location))
 importScripts('../web.bundle.js')
 postMessage("dotlit: " + typeof dotlit)
@@ -19,13 +19,3 @@ const getFsTest = async (filepath) => {
 }
 
 getFsTest('/index.lit')
-```
-```>txt attached=true updated=1620077687465 !error
-function toString() {
-    [native code]
-}
-worker.js:6:14
-worker.js:24:63
-module code@worker.js:24:139
-evaluate@[native code]
-moduleEvaluation@[native code]
