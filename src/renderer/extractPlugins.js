9de0900a5459bbb6fbaafe65af21ecf4215c37d1
dotlitdev <webmaster@dotlit.org>
2021-05-03 21:38:03 +0100

    plugins

---
+++ src/renderer/extractPlugins.js
@@ -0,0 +1,146 @@
+import path from 'path'
+import { getConsoleForNamespace } from '../utils/console'
+import { selectAll } from 'unist-util-select'
+import {btoa, atob } from '../utils/safe-encoders'
+
+const console = getConsoleForNamespace('plugins')
+   
+
+const extractModule = async (src, filename) => {
+    if (typeof global !== 'undefined'){
+        var Module = module.constructor;
+        var m = new Module();
+
+        if (typeof m._compile === 'function') {
+            return {}
+        //     m._compile(src, filename);
+        //     console.log("Compiled as commonjs module", m, m.exports)
+        //     if (m.exports && Object.keys(m.exports).length) return m.exports;
+        //     else throw new Error("No module.exports when loaded as commonjs")
+        }
+        
+    }
+    console.log("Importing as es6 module via data:uri import.")
+    // const blobUrl = URL.createObjectURL(new Blob([src], {type: 'text/javascript'}))
+    // return await import(/* webpackIgnore: true */ blobUrl)
+    return await import(/* webpackIgnore: true */ `data:text/javascript;base64,${ btoa(src)}`)
+}
+
+export const extractPlugins = ({fs} = {}) => {
+    return async (tree,file) => {
+        console.log("Checking for plugins")
+        file.data = file.data || {}
+        file.data.plugins = {}
+
+        for (const block of selectAll("code", tree)) {
+
+            const filename = (block.data
+                             && block.data.meta
+                             && block.data.meta.filename) || ''
+            // viewer
+            if (block.data 
+                && block.data.meta 
+                && block.data.meta.directives
+                && block.data.meta.directives.indexOf('viewer') >= 0) {
+                
+                console.log('Found Viewer', block.meta)
+                file.data = file.data || {}
+                file.data.viewers = file.data.viewers || {}
+                try {
+                    let viewer = await extractModule(block.value, filename)
+                    if (viewer.asyncViewer) {
+                        viewer = await viewer.asyncViewer()
+                    } else if (viewer.viewer) {
+                    } else {
+                        console.log(viewer)
+                        throw new Error("No viewer exported from module")
+                    }
+                    file.data.viewers[block.data.meta.of] = viewer
+                   
+                } catch(err) {
+                    console.log("Failed to init viewer", err)
+                    const msg = "Viewer Error: " + (err.message || err.toString())
+                    file.data.viewers[block.data.meta.of] 
+                     = () => msg
+                    file.message(msg, block)
+                }
+            }
+            // transformer
+            if (block.data 
+                && block.data.meta 
+                && block.data.meta.directives
+                && block.data.meta.directives.indexOf('transformer') >= 0) {
+                
+                console.log('Found Transformer', block.meta)
+                file.data = file.data || {}
+                file.data.transformers = file.data.transformers || {}
+                try {
+                    let transformer = await extractModule(block.value, filename )
+                    if (transformer.asyncTransformer) transformer = await transformer.asyncTransformer()
+                    else if (transformer.transformer) {
+                    } else {
+                        console.log(transformer)
+                        throw new Error("No transformer exported from module")
+                    }
+                    file.data.transformers[block.data.meta.of] = transformer
+                   
+                } catch(err) {
+                    console.log("Failed to init transformer", err)
+                    const msg = "Transformer Error: " + (err.message || err.toString())
+                    file.data.transformers[block.data.meta.of] 
+                     = () => msg
+                    file.message(msg, block)
+                }
+            }
+
+            // Generic plugins
+            if (block.data 
+                && block.data.meta 
+                && block.data.meta.directives
+                && block.data.meta.directives.indexOf('plugin') >= 0) {
+                
+                const meta = block.data.meta
+                console.log('Found Plugin', meta)
+                
+                let type = meta.type || 'unknown'
+                const types = ['parser', 'renderer', 'transformer', 'viewer', 'unknown']
+                
+                file.data = file.data || {}
+                file.data.plugins = file.data.plugins || {}
+                file.data.plugins[type] = file.data.plugins[type] || {}
+                
+                const len = Object.keys(file.data.plugins[type]).length
+                const id = meta.of || meta.id || meta.filename || len
+                
+                try {
+                    let plugin = await extractModule(block.value, filename)
+                    console.log("plugin module:", plugin)
+                    let foundExport;
+                    if (plugin.asyncPlugin) {
+                        plugin = await plugin.asyncPlugin()
+                    }
+
+                    for (const type of types) {
+                        if (plugin[type]) {
+                            foundExport = true
+                            if (file.data.plugins[type] && file.data.plugins[type][id]) {
+                                file.message(`Duplicate plugin for type: ${type} id: ${id}.`, block)
+                            } else {
+                                file.data.plugins[type] = file.data.plugins[type] || {}
+                                file.data.plugins[type][id] = plugin[type]
+                            }
+                        }
+                    }
+
+                    if (!foundExport) throw new Error(`No plugin exported from module. for ${block.meta}`)
+                    
+                   
+                } catch(err) {
+                    console.error("Failed to init plugin", err)
+                    const msg = `Plugin Error (${type}:${id}): ` + (err.message || err.toString())
+                    file.message(msg, block)
+                }
+            }
+        }
+    }
+}
