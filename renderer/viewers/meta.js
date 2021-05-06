72a093176d9fd236ccf8f1e2c15053db593649f7
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-06 14:19:29 +0000

    Deploying to gh-pages from  @ 0ee79274e3f60dab32924fa88db9ae3aa36baf09 🚀

--- renderer/viewers/meta.js
+++ renderer/viewers/meta.js
@@ -1,5 +1,6 @@
 import diff from 'https://cdn.skypack.dev/diff'
 
 export const viewer = ({node,React}) => {
+  console.log("DIFF module", diff)
   return React.createElement('pre', {style: {color: 'black'}}, lit.utils.inspect(diff))
 }
