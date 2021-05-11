b6b126b50786bcc863fbb224f0de9fc9440f99fa
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-11 13:11:57 +0100

    Updated src/testing/log/checkforinput.js

--- src/testing/log/checkforinput.js
+++ src/testing/log/checkforinput.js
@@ -16,9 +16,9 @@ const checkForInput = async () => {
     window.history.replaceState(null,null,'?' + qsWoInput)
 
     let stat = {local:{},remote:{}} 
-    try { stat = await lit.fs.readStat(filename + ".lit", {encodin: 'utf8'}) } catch(err) {}
+    try { stat = await lit.fs.readStat(`/${filename}.lit`, {encodin: 'utf8'}) } catch(err) {}
     const newContent = (stat.local.value || stat.remote.value || `# ${today}`) + ("\n" + input)
-    await lit.fs.writeFile(filename + ".lit", newContent)
+    await lit.fs.writeFile(`/${filename}.lit`, newContent)
     return `***Captured Input (below) to [[${filename}]]***
 
 ${input}`
