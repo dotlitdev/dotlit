a3363181b879684220b9cf2d2c22bacd7492ad95
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-15 00:23:26 +0100

    Updated src/testing/runkit-repl-endpoint.js

--- src/testing/runkit-repl-endpoint.js
+++ src/testing/runkit-repl-endpoint.js
@@ -7,10 +7,10 @@ exports.endpoint = function(req,res) {
   });
 
   function requireFromString(src, filename) {
-  var Module = module.constructor;
-  var m = new Module();
-  m._compile(src, filename);
-  return m.exports;
+    var Module = module.constructor;
+    var m = new Module();
+    m._compile(src, filename);
+    return m.exports;
 }
 
   let data = '';
