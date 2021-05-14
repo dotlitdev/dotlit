e1fb32d10858cd3c50edbaed6830b0aa5f10b5d0
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-14 23:23:49 +0000

    Deploying to gh-pages from  @ 5937709f420e4b425d3ea36a93b585a3b80567ef 🚀

--- testing/test.js
+++ testing/test.js
@@ -1,3 +1,3 @@
 module.exports = async (meta) => {
-  return `Hello world! From .lit and Nodejs ${process.env.NODE_VERSION} (${process.platform} ${process.arch}) thanks to RunKit.`
+  return `Hello world! From .lit and Nodejs ${process.env.NODE_VERSION} (${process.platform} ${process.arch}) thanks to RunKit. At ${new Date()}`
 }
