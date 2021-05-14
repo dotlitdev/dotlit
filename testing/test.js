c8d51e1651e19f8c6f0a1a5ac6088540321ff546
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-14 23:07:14 +0000

    Deploying to gh-pages from  @ b7c77a2cfd31a2936338b02fa9fa5976475ce292 🚀

--- testing/test.js
+++ testing/test.js
@@ -1,10 +1,8 @@
-
 const info = {
   text: `Hello world! From .lit and Nodejs (${process.platform} ${process.arch}) thanks to RunKit.`,
   cwd: process.cwd(),
   env: process.env,
-  
 }
 module.exports = async (meta) => {
-  return 42
+  return info.env
 }
