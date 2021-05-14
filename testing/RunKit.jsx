f2c943e6485d5b5d9523168a7334b4655c9df8d1
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-14 16:35:22 +0000

    Deploying to gh-pages from  @ e316731fb94f60a747b097b013faae9d66bb7f42 🚀

---
+++ testing/RunKit.jsx
@@ -0,0 +1,17 @@
+exports const viewer = ({node,React}) => {
+
+  const Embed = (await import('https://cdn.skypack.dev/runkit-embed-react')).default
+
+  const onLoad = (...args) => alert(JSON.stringify([...args]))
+
+  return <Embed
+            mode='endpoint'
+            readOnly={true}
+            evaluateOnLoad={true}
+            // hidesActionButton={true}
+            source={ node.data.value } 
+            // ref='embed'
+            onLoad={ onLoad } 
+          />
+
+}
