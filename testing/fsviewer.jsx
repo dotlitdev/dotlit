b9dd73e4400869492efc3136383eca5acf8d6c02
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-15 23:03:49 +0000

    Deploying to gh-pages from  @ b87e9a4d26933a44a85cb56bab55a1541874323f 🚀

--- testing/fsviewer.jsx
+++ testing/fsviewer.jsx
@@ -1,18 +1,11 @@
 export const viewer = ({node, React}) => {
-
-  const Stat = ({stat}={}) => {
-  return <div>
-    Type: <span>{stat.type}</span>
-    mtime: <span>{stat.mtimeMs}</span>
-  </div>
-}
-
-
   const {useState, useEffect} = React
+
   const [content, setContent] = useState(<span>loading...</span>)
   // const [stat, setStat] = useState(undefined)
   const [src, setSrc] = useState(node.data.value.trim())
   const meta = node.properties.meta
+
   useEffect(async fn => {
     let stat
     try {
@@ -22,6 +15,7 @@ export const viewer = ({node, React}) => {
       setContent(<span>{err.message}</span>)
     }
   }, [src])
+
   return <div>
      <input value={src} onChange={ev=>setSrc(ev.target.value)}/>
      <Stat stat={stat}/>
