9f4c183f458e8efcd9c024b1b74bea65ca2bb0b0
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-15 23:02:17 +0000

    Deploying to gh-pages from  @ b85fe00f0357b62c6d8e73f16fcaea8e0065994d 🚀

--- testing/fsviewer.jsx
+++ testing/fsviewer.jsx
@@ -1,15 +1,16 @@
+export const viewer = ({node, React}) => {
 
-const Stat = ({stat}={}) => {
+  const Stat = ({stat}={}) => {
   return <div>
     Type: <span>{stat.type}</span>
     mtime: <span>{stat.mtimeMs}</span>
   </div>
 }
 
-export const viewer = ({node, React}) => {
+
   const {useState, useEffect} = React
   const [content, setContent] = useState(<span>loading...</span>)
-  const [stat, setStat] = useState(undefined)
+  // const [stat, setStat] = useState(undefined)
   const [src, setSrc] = useState(node.data.value.trim())
   const meta = node.properties.meta
   useEffect(async fn => {
