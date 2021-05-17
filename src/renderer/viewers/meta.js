b77bd0fd9baa43ce3b8dbfb5e20140278f8e7b33
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-17 21:39:06 +0100

    Updated src/renderer/viewers/meta.js

--- src/renderer/viewers/meta.js
+++ src/renderer/viewers/meta.js
@@ -1,6 +1,3 @@
-import diff from 'https://cdn.skypack.dev/diff'
-
-export const viewer = ({node, React, lit}) => {
-  console.log("DIFF module", diff)
-  return React.createElement('pre', {style: {color: 'black'}}, lit.utils.inspect(diff))
+export const viewer = ({node,React}) => {
+  return React.createElement('pre', {style: {color: 'black'}}, lit?.utils?.inspect(node))
 }
