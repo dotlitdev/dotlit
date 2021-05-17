249bbeb2b4532ce24607964e5dd4b55dc570353f
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-17 20:40:17 +0000

    Deploying to gh-pages from  @ b77bd0fd9baa43ce3b8dbfb5e20140278f8e7b33 🚀

--- renderer/viewers/meta.js
+++ renderer/viewers/meta.js
@@ -1,3 +1,3 @@
-export const viewer = ({node,React, lit}) => {
+export const viewer = ({node,React}) => {
   return React.createElement('pre', {style: {color: 'black'}}, lit?.utils?.inspect(node))
 }
