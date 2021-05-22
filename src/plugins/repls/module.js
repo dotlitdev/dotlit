6d712d32a6bea001ffe9ce2f3d18fdc7ca3d0066
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-22 14:26:04 +0100

    Updated src/plugins/repls/module.js

--- src/plugins/repls/module.js
+++ src/plugins/repls/module.js
@@ -6,11 +6,19 @@ export const repl = async (src, meta) => {
   try {
     babel = transform(filename, src);
 
-    const s = `/*${Date.now()}*/` + babel.code;
+    // So many hacks due to blob and/or data uri
+    // - cachbusting comment
+    // - rewrite imports urls to be absolute
+    const s =
+      `/*${Date.now()}*/` +
+      babel.code.replace(
+        "HORRIBLE_HACK",
+        new URL("..", location.href).toString()
+      );
     const console = "fake me";
     // const url = `data:text/javascript;base64,${btoa(s)}`
     const url = URL.createObjectURL(new Blob([s], { type: "text/javascript" }));
-    const m = await import(url + "#path=" + lit.location.src);
+    const m = await import(url + "#location=" + location.href);
     if (typeof m.default === "function") {
       const res = await m.default.call({ console });
       return lit.utils.inspect(res);
