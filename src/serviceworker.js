167bcd422148410788ba8a67bf6e889128ca2da1
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-22 13:14:48 +0100

    Updated src/serviceworker.js

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -4,7 +4,7 @@ let document = { documentElement: { style: {} } };
 importScripts("web.bundle.js");
 
 const state = {
-  version: "0.2.9",
+  version: "0.2.10",
   dotlit: typeof dotlit,
   root: "",
   enableCache: false,
