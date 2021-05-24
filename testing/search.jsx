e51054addbba931563d3dda4aac0fee4dfdcaf66
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-24 21:45:26 +0000

    Deploying to gh-pages from  @ 57b72326b1e3eb3068d64ddac316fbc8241c3fd4 🚀

---
+++ testing/search.jsx
@@ -0,0 +1,86 @@
+export const repl = async (src, meta) => {
+  const t = Date.now();
+
+  const { default: Fuse } = await import(
+    "https://cdn.jsdelivr.net/npm/fuse.js@6.4.6/dist/fuse.esm.js"
+  );
+  // const manifest = await fetch("/manifest.json").then((res) => res.json());
+
+  // Recursively builds JSX output adding `<mark>` tags around matches
+  const highlight = (value, indices = [], i = 1) => {
+    const pair = indices[indices.length - i];
+    return !pair ? (
+      value
+    ) : (
+      <>
+        {highlight(value.substring(0, pair[0]), indices, i + 1)}
+        <mark>{value.substring(pair[0], pair[1] + 1)}</mark>
+        {value.substring(pair[1] + 1)}
+      </>
+    );
+  };
+
+  const fullLocal = await (async (fn) => {
+    const path = lit.utils.path;
+    const all = [];
+    const visit = async (root) => {
+      try {
+        const list = await lit.fs.readdir(root);
+        return Promise.all(
+          list.map(async (key) => {
+            const pathname = path.join(root, key);
+            const stat = await lit.fs.stat(pathname);
+            let contents;
+            if (key === ".git" || !key) {
+            } else if (stat.type === "dir") await visit(pathname);
+            else if (pathname.endsWith(".lit"))
+              contents = await lit.fs.readFile(pathname, {
+                encoding: "utf8",
+                localOnly: true,
+              }); //.slice(0,10);
+            const item = { pathname, type: stat.type, contents };
+            all.push(item);
+            return item;
+          })
+        );
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+
+    await visit("/");
+    return all;
+  })();
+
+  // return fullLocal
+
+  const fuse = new Fuse(fullLocal, {
+    ignoreLocation: true,
+    includeScore: true,
+    includeMatches: true,
+    ignoreFieldNorm: true,
+    minMatchCharLength: 4,
+    keys: ["pathname", "contents"],
+  });
+
+  // 3. Now search!
+  const query = src.trim();
+  const msg = `Results for search "**${query}**". In **${
+    (Date.now() - t) / 1000
+  }** seconds.\n\n`;
+
+  return (
+    msg +
+    fuse
+      .search(query, { limit: 10 })
+      //.map(x=>x.matches.map(x=>x.indices))
+      .map((x) => [x.score, x.item.pathname, x.refIndex, x.matches])
+      .map(
+        ([score, pathname, index, matches]) =>
+          `1. [${pathname}](${pathname}) 
+    
+      > ${JSON.stringify(matches)}`
+      )
+      .join("\n")
+  );
+};
