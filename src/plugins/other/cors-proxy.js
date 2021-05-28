98fc11aca97a6aec049adec4ed324486ce12459c
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-28 22:19:23 +0100

    Updated src/plugins/other/cors-proxy.js

---
+++ src/plugins/other/cors-proxy.js
@@ -0,0 +1,27 @@
+if (typeof lit !== "undefined" && !window.__runkitCORSProxyEnpoint) {
+  (async (fn) => {
+    const el = document.createElement("div");
+    document.body.appendChild(el);
+    el.setAttribute("style", "height:0;");
+    RunKit.createNotebook({
+      element: el,
+      mode: "endpoint",
+      onLoad: async (rk) => {
+        window.__runkitCORSProxyEnpoint = await rk.getEndpointURL();
+        document.body.removeChild(el);
+      },
+      evaluateOnLoad: true,
+      source: await lit.fs.readFile("/testing/runkit-repl-endpoint.js", {
+        encoding: "utf8",
+      }),
+    });
+  })();
+}
+
+export const proxy = async (src, meta, node) => {
+  if (!window.__runkitCORSProxyEnpoint) {
+    return "Still setting up proxy endpoint";
+  } else {
+    return window.__runkitCORSProxyEnpoint;
+  }
+};
