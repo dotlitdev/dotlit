608c53a51befd1dafcbd6bdad128419b12eb67b4
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-29 00:48:34 +0100

    Updated src/testing/runkit-express-cors-proxy.js

--- src/testing/runkit-express-cors-proxy.js
+++ src/testing/runkit-express-cors-proxy.js
@@ -1,11 +1,15 @@
 const express = require("@runkit/runkit/express-endpoint/1.0.0");
 const cors = require("cors");
-const request = require("request");
+const request = require("request").defaults({ maxRedirects: 100 });
 
 const app = express(exports);
 app.use(cors());
 app.use("/:path", (req, res) => {
-  const target = req.header("Target-Domain");
-  res.end("WIP: " + target + req.url);
-  // req.pipe(request(target + req.url)).pipe(res);
+  try {
+    const target = req.header("Target-Domain");
+    //res.end("WIP: " + target + req.url);
+    req.pipe(request(target + req.url)).pipe(res);
+  } catch (err) {
+    res.end(err.message);
+  }
 });
