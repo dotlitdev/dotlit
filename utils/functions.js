8a90adc53fe5aae178c41d32bb86d678090169a6
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-29 23:39:34 +0000

    Deploying to gh-pages from  @ e4a4ac131d3606e80d27a65e47e25909a547f63f 🚀

--- utils/functions.js
+++ utils/functions.js
@@ -1,31 +1,8 @@
 export const NoOp = () => {}
 export const Identity = x => x
 export const AsInt = x => parseInt(x)
-export const Undef = x => typeof x === 'undefined'
-
 export const getMeta = (key,def) => {
-    if (typeof document === "undefined" || !document.querySelector) return def;
-
     const el = document.querySelector(`meta[name="lit${key}"]`)
     const val = el ? el.getAttribute('value') : def
     return val
-}
-
-export const posstr = pos => {
-    return pos ? `${pos.line}:${pos.column}-${pos.offset}` : undefined
-}
-
-export const wait = async (ms) => {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms);
-    });
-}
-
-export const template = (templateString, templateVars) => {
-    console.log(templateString)
-    const escaped = templateString.replace(/`/g, '\\`')
-    console.log(escaped)
-    const body = "return `"+ escaped +"`;"
-    console.log(body)
-    return new Function(body).call(templateVars)
 }
