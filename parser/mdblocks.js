616e2111dd0f6312b9f380a0d115b3c6be022e42
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-05-30 20:53:15 +0000

    Deploying to gh-pages from  @ 94514ee7d894354905f7bee67b83cdd2a5ce7c55 🚀

--- parser/mdblocks.js
+++ parser/mdblocks.js
@@ -1,6 +1,6 @@
-
 import visit from 'unist-util-visit'
 import {getConsoleForNamespace} from '../utils/console'
+import {vfile} from 'vfile'
 
 const console = getConsoleForNamespace('mdblocks')
 
@@ -16,9 +16,10 @@ export const mdblocks = function ({baseProcessor, files, litroot}) {
             // instead of await (why?)
             const p = new Promise(async resolve => {
                 // console.log(idx + "Node: ", node)
+                const mdfile = new vfile({path: file.data.canonical, value: node.value})
                 const p = baseProcessor({files, litroot})
-                const parsed = await p.parse( node.value )
-                const ast = await p.run(parsed)
+                const parsed = await p.parse( mdfile )
+                const ast = await p.run(parsed, mdfile)
                 //  console.log(idx + " AST: ", ast)
                 node.children = ast.children
                 resolve()
