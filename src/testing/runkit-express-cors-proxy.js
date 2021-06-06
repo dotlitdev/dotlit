569b5f18ffcad11168431cd3112ac7822f7ec18c
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-06-06 13:10:27 +0100

    Edited src/testing/runkit-express-cors-proxy.js

--- src/testing/runkit-express-cors-proxy.js
+++ src/testing/runkit-express-cors-proxy.js
@@ -6,8 +6,8 @@ const app = express(exports);
 app.use(cors());
 app.use("/", (req, res) => {
   try {
-    //req.pipe(request('https://' + req.url)).pipe(res);
-    res.end(req.url);
+    req.pipe(request("https://" + req.url.slice(1))).pipe(res);
+    //  res.end(req.url);
   } catch (err) {
     res.end(err.message);
   }
