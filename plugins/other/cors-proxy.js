a08e8a239ef8db5278798c976b345152be828ec2
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-06-06 12:35:21 +0000

    Deploying to gh-pages from  @ 596fbe66f35a487847693d997010fcdd985f2a3c 🚀

--- plugins/other/cors-proxy.js
+++ plugins/other/cors-proxy.js
@@ -1,31 +1,32 @@
-
-const setup() {
-if (typeof lit !== "undefined" && !window.__runkitCORSProxyEnpoint) {
-  (async (fn) => {
-    const rkEmbed = document.createElement("script");
-    rkEmbed.onload = async (fn) => {
-      const el = document.createElement("div");
-      document.body.appendChild(el);
-      el.setAttribute("style", "height:0;");
-      RunKit.createNotebook({
-        element: el,
-        mode: "endpoint",
-        onLoad: async (rk) => {
-          window.__runkitCORSProxyEnpoint = await rk.getEndpointURL();
-          document.body.removeChild(el);
-        },
-        evaluateOnLoad: true,
-        source: await lit.fs.readFile("/testing/runkit-express-cors-proxy.js", {
-          encoding: "utf8",
-        }),
-      });
-    };
-    rkEmbed.setAttribute("src", "https://embed.runkit.com");
-    document.body.appendChild(rkEmbed);
-  })();
-}
-
-}
+const setup = new Promise((resolve, reject) => {
+  if (typeof lit !== "undefined" && !window.__runkitCORSProxyEnpoint) {
+    (async (fn) => {
+      const rkEmbed = document.createElement("script");
+      rkEmbed.onload = async (fn) => {
+        const el = document.createElement("div");
+        document.body.appendChild(el);
+        el.setAttribute("style", "height:0;");
+        RunKit.createNotebook({
+          element: el,
+          mode: "endpoint",
+          onLoad: async (rk) => {
+            window.__runkitCORSProxyEnpoint = await rk.getEndpointURL();
+            document.body.removeChild(el);
+          },
+          evaluateOnLoad: true,
+          source: await lit.fs.readFile(
+            "/testing/runkit-express-cors-proxy.js",
+            {
+              encoding: "utf8",
+            }
+          ),
+        });
+      };
+      rkEmbed.setAttribute("src", "https://embed.runkit.com");
+      document.body.appendChild(rkEmbed);
+    })();
+  }
+});
 
 export const proxy = async (returnEndpoint) => {
   if (!window.__runkitCORSProxyEnpoint) {
