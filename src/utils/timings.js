3fe6a4f330896d22801d019d0c43af8b7085c849
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-06-26 23:14:49 +0100

    Create timings.js

---
+++ src/utils/timings.js
@@ -0,0 +1,22 @@
+const timings = {}
+let prev = ((typeof localStorage !== 'undefined') && localStorage.getItem('litTimings')) || ''
+export const time = (ns, marker) => {
+    const now = Date.now()
+    timings[ns] = timings[ns] || {
+        start: now,
+        marks: [],
+        timeTo: {},
+    }
+    timings[ns].marks.push({marker,time: now})
+    if (marker) {
+        const id = now
+        const took = now - timings[ns].start 
+        timings[ns].timeTo[marker] = took
+        console.log(`[timings][${ns}] "start" to "${marker}" took ${took}ms`)
+        const log = JSON.stringify({ns,marker,id,took}) + "\n"
+        prev += log
+        if (typeof localStorage !== 'undefined') localStorage.setItem('litTimings', prev)
+    }
+}
+
+export const getTimings = () => timings
