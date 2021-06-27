d8056af8b34b131447e19413a08c9231e6b924a4
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-06-27 14:33:20 +0100

    Edited src/utils/timings.js

--- src/utils/timings.js
+++ src/utils/timings.js
@@ -1,24 +1,27 @@
-const timings = {}
+const timings = {};
 
-export const getPrev = () => ((typeof localStorage !== 'undefined') && localStorage.getItem('litTimings')) || ''
+export const getPrev = () =>
+  (typeof localStorage !== "undefined" && localStorage.getItem("litTimings")) ||
+  "";
 
 export const time = (ns, marker) => {
-    const now = Date.now()
-    timings[ns] = timings[ns] || {
-        start: now,
-        marks: [],
-        timeTo: {},
-    }
-    timings[ns].marks.push({marker,time: now})
-    if (marker) {
-        const id = now
-        const took = now - timings[ns].start 
-        timings[ns].timeTo[marker] = took
-        console.log(`[timings][${ns}] "start" to "${marker}" took ${took}ms`)
-        const log = JSON.stringify({ns,marker,id,took}) + "\n"
-   
-        if (typeof localStorage !== 'undefined') localStorage.setItem('litTimings', getPrev() + log)
-    }
-}
+  const now = Date.now();
+  timings[ns] = timings[ns] || {
+    start: now,
+    marks: [],
+    timeTo: {},
+  };
+  timings[ns].marks.push({ marker, time: now });
+  if (marker) {
+    const id = now;
+    const took = now - timings[ns].start;
+    timings[ns].timeTo[marker] = took;
+    console.log(`[timings][${ns}] "start" to "${marker}" took ${took}ms`);
+    const log = JSON.stringify({ ns, marker, id, took }) + "\n";
 
-export const getTimings = () => timings
+    if (typeof localStorage !== "undefined")
+      localStorage.setItem("litTimings", getPrev() + log);
+  }
+};
+
+export const getTimings = () => timings;
