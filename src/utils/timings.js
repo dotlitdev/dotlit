41ae254f8181df628645403a075ea1816c802dda
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-06-27 21:38:49 +0100

    Edited src/utils/timings.js

--- src/utils/timings.js
+++ src/utils/timings.js
@@ -6,21 +6,21 @@ export const getPrev = () =>
 
 export const time = (ns, marker) => {
   const now = Date.now();
-  timings[ns] = (!marker || !timings[ns])
-    ? {
+  timings[ns] = !marker
+    ? timings[ns]
+    : {
         start: now,
         marks: [],
         timeTo: {},
-      }
-    : timings[ns];
-
+      };
   timings[ns].marks.push({ marker, time: now });
   if (marker) {
     const id = now;
     const took = now - timings[ns].start;
+    const index = timings[ns].marks.length;
     timings[ns].timeTo[marker] = took;
     console.log(`[timings][${ns}] "start" to "${marker}" took ${took}ms`);
-    const log = JSON.stringify({ ns, marker, id, took }) + "\n";
+    const log = JSON.stringify({ ns, marker, id, took, index }) + "\n";
 
     if (typeof localStorage !== "undefined")
       localStorage.setItem("litTimings", getPrev() + log);
