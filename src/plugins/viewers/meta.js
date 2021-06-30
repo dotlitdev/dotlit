5d04d4df0e2226b39d46ffb585e7644c410df3bb
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-06-30 10:06:18 +0100

    Edited src/plugins/viewers/meta.js

--- src/plugins/viewers/meta.js
+++ src/plugins/viewers/meta.js
@@ -2,7 +2,7 @@ export const viewer = ({ node, React }) => {
   const { useEffect, useState } = React;
   const [output, setOutput] = useState("Loading...");
   useEffect(() => {
-    setOutput(JSON.stringify(node,null,2));
+    setOutput(JSON.stringify(node, null, 2));
   }, [node]);
   return React.createElement("pre", { style: { color: "black" } }, output);
 };
