a27ca31a49d38f482daeb8f2badc1d438e1aa8e0
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2021-07-04 22:10:36 +0000

    Deploying to gh-pages from @ dotlitdev/dotlit@e09f9f7f35c620dd5672536865b08abafa805384 🚀

--- components/Cell.jsx
+++ components/Cell.jsx
@@ -58,7 +58,7 @@ const Cell = props => {
     const rawSource = codeSource && ("```" + (meta.raw || '') + "\n" + codeSource + "```")
 
     const isTranscluded = meta?.source?.filename
-    const originalSource = meta && ("```" + (meta.raw || '') + "\n" + codeNode.value + "```")
+    const originalSource = meta && ("```" + (meta.raw || '') + "\n" + (isTranscluded ? codeNode.data.originalSource : codeNode.data.value) + "```")
     
     const output = meta && meta.isOutput
 
@@ -118,8 +118,9 @@ const Cell = props => {
             }
             const outputMeta = (meta.hasOutput ? meta.output.raw : 'txt').trim() + (" attached=true updated=" + Date.now()) + (error ? ' !error' : '')
             const output = "\n```>"+ outputMeta +"\n" + result.stdout.replace(/\n```/g, "\n•••") + "\n```\n"
-            if (ctx) ctx.setSrc(pos, rawSource + output)
-            else return rawSource + output
+            const src = isTranscluded ? originalSource : rawSource
+            if (ctx) ctx.setSrc(pos, src + output)
+            else return src + output
         }
     }
 
