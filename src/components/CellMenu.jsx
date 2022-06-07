b075a7ab2d02661e7e6915c5bf7dc7530565748f
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2022-06-07 12:23:06 +0100

    add undo icon

--- src/components/CellMenu.jsx
+++ src/components/CellMenu.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import { MenuIcon, EditIcon, ExecIcon, CloseIcon, SaveIcon, TrashIcon } from './Icons'
+import { MenuIcon, EditIcon, ExecIcon, CloseIcon, SaveIcon, TrashIcon, CancelUndoIcon } from './Icons'
 import { getConsoleForNamespace } from '../utils/console'
 
 const console = getConsoleForNamespace('CellMenu')
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faPlay, faEdit, faExternalLinkAlt, faTimes, faSave, faAnchor, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faPlay, faEdit, faExternalLinkAlt, faTimes, faSave, faAnchor, faTrash, faUndo } from '@fortawesome/free-solid-svg-icons'
 
 
 export const Icon = (props) => <FontAwesomeIcon icon={props.icon} />
@@ -11,6 +11,6 @@ export const ExecIcon = props => (<FontAwesomeIcon icon={faPlay} {...props} />)
 export const CloseIcon = props => (<FontAwesomeIcon icon={faTimes} {...props} />)
 export const SaveIcon = props => (<FontAwesomeIcon icon={faSave} {...props} />)
 export const TrashIcon = props => (<FontAwesomeIcon icon={faTrash} {...props} />)
-
+export const CancelUndoIcon = props => <Icon icon={faUndo} />
 export const ExternalLinkIcon = props => <Icon icon={faExternalLinkAlt}/>
 export const AnchorIcon = props => <Icon icon={faAnchor}/>
