f5340974c41dc81d9815bd3a76b90e66afe27504
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2022-06-17 22:15:38 +0000

    Deploying to gh-pages from @ dotlitdev/dotlit@d28717e128cf637649e94ff26db55541e74852e7 🚀

--- parser/sections-v3.js
+++ parser/sections-v3.js
@@ -1,2 +1,35 @@
-// Code TBD from above.
-// This files source is defined in [[testing/section_grouping.lit]]
+// This files source is defined in [[testing/section_grouping|🔬 Testing Section grouping]]
+
+export const sections = (...args) => (tree) => {
+  const stack = [tree]
+  const nodes = tree.children
+  tree.children = []
+
+  const createSection = (node) => {
+    const section = {
+      type: 'section',
+      children: [node],
+      depth: node.depth || 0,
+    }
+    stack.push(section)
+  }
+
+  for (const node in nodes) {
+    const section = stack[-1]
+    if (section.type === 'root') {
+      createSection(node)
+    } else if (node.type === 'heading') {
+      if (node.depth > section.depth) {
+        createSection(node)
+      } else {
+        while (stack[-1].depth > node.depth ) {
+          stack.pop()
+        }
+        createSection(node)
+      }
+    } else {
+      section.children.push(node)
+    }
+  }
+
+}
