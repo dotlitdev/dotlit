ccf56d8b3e0d7b4949213ffd0a67ac86bad8f8e8
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2022-06-18 13:26:21 +0100

    Edited src/parser/sections-v3.js

--- src/parser/sections-v3.js
+++ src/parser/sections-v3.js
@@ -1,6 +1,6 @@
 // This files source is defined in [[testing/section_grouping|🔬 Testing Section grouping]]
 
-export const sections = (...args) => (tree) => {
+export const sections = ({processSection}) => (tree) => {
   const stack = [tree]
   const nodes = tree.children
   tree.children = []
@@ -11,26 +11,33 @@ export const sections = (...args) => (tree) => {
       children: [node],
       depth: node.depth || 0,
     }
-    // TODO? push section to stack[-1].children
+    stack.at(-1).children.push(section)
     stack.push(section)
   }
 
   for (const node in nodes) {
-    const section = stack[-1]
+    const section = stack.at(-1)
     if (section.type === 'root') {
       createSection(node)
     } else if (node.type === 'heading') {
       if (node.depth > section.depth) {
         createSection(node)
       } else {
-        // TODO? stop before popping root?
-        while (stack[-1].depth > node.depth ) {
-          stack.pop()
+        while (stack.at(-1).type !== 'root' && stack.at(-1).depth >= node.depth ) {
+          const s = stack.pop()
+          // processSection(s)
         }
         createSection(node)
       }
     } else {
       section.children.push(node)
+    }
+
+    if (!nodes[index+1]) {
+      while (stack.at(-1).type !== 'root' && stack.at(-1).depth >= node.depth ) {
+          const s = stack.pop()
+          // processSection(s)
+        }
     }
   }
 
