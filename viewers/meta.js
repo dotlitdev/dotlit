47531fbe834cbd6c4a05abd32fc19027d7f9def7
dotlitdev <73314508+dotlitdev@users.noreply.github.com>
2022-06-19 22:23:40 +0000

    Deploying to gh-pages from @ dotlitdev/dotlit@aa2d5d87c9ee8257c14d868a98bf1b60e49ea6c9 🚀

--- viewers/meta.js
+++
@@ -1,8 +0,0 @@
-export const viewer = ({node,React}) => {
-  const {useEffect, useState} = React
-  const [output,setOutput] = useState("Loading...")
-  useEffect(()=>{
-    setOutput(lit.utils.inspect(node))
-  }, [node])
-  return React.createElement('pre', {style: {color: 'black'}}, output)
-}
